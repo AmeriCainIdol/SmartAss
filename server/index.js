24d6b95150d6a2a0dbfa410d74751cea43ca7ed6
jvalamis <jvalamis@gmail.com>
2018-09-11 16:35:19 -0500

    morgan, bodyparser, nodemon, config added to gitignore, trivia api helpers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,42 @@
 const express = require('express');
+//morgan will intercept http requests and log them in terminal
+const morgan = require('morgan');
+//body parser
+const bodyParser = require('body-parser');
+//nodemon
+const nodemon = require('nodemon')
 
 const app = express();
+//use the tiny version of morgan showing the http method and ping
+app.use(morgan('tiny'));
+//bodyParser config options
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+ 
+
 
 let port = process.env.PORT || 3030;
 
 app.listen(app, function () {
   console.log(`listening on port ${port}`)
 })
+
+//handler for signing up
+app.post('/signup',
+  () => {
+    //take the info from the fields
+    //save them to the database
+    //redirect user to login page
+
+  });
+
+//handler for logging in
+app.get('/login',
+  () => {
+    //search database for user
+    //authenticate user session
+    //redirect to game page
+  })
+
