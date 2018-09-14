d6166b4c1cbb1877fa3255bfd8e44e709c74c3a8
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-14 17:01:33 -0500

    moved home page rendering from index.jsx to home.jsx

---
+++ front-end/src/components/home.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SignUp from './sign_up.jsx';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <Router>
+      <div className="container-fluid">
+        <h1>Smart-Ass™</h1>
+        <h2>A Trivia Game</h2>
+        <h3>© 2018</h3>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-8">
+                <h3>Leader Board</h3>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Rank</th>
+                      <th>Username</th>
+                      <th>Wins</th>
+                      <th>Losses</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Jstrzesz</td>
+                      <td>4</td>
+                      <td>0</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className="col-md-4">
+                <h3>Log In</h3>
+                <form role="form">
+                  <div className="form-group">
+                    <label>
+                      Username
+                  </label>
+                    <input type="email" className="form-control" id="UsernameInput" />
+                  </div>
+                  <div className="form-group">
+                    <label>
+                      Email address
+                  </label>
+                    <input type="email" className="form-control" id="EmailInput" />
+                  </div>
+                  <div className="form-group">
+                    <label>
+                      Password
+                  </label>
+                    <input type="password" className="form-control" id="PasswordInput" />
+                  </div>
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+                </form>
+                <h4>New Players</h4>
+                <h5>Not a player? Sign Up Here </h5>
+                <Link to="/sign_up">
+                  <button type="submit" className="btn btn-primary">Sign Up</button>
+                  <Route path="/sign_up" component={SignUp} />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </Router>
+    )
+  }
+
+}
+
+export default Home;
--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/home.jsx';
 import SignUp from './components/sign_up.jsx';
 import GameOver from './components/gameOver.jsx';
 import GamePage from './components/gamePage.jsx';
@@ -20,108 +21,7 @@ class App extends React.Component {
 
   render () {
     return (
-      // <div className="container-fluid">
-      //   <div className="row">
-      //     <div className="col-md-12">
-      //       <h2>Question #</h2>
-      //       <p>What real-world event is thought to be the inspiration for the destruction of Atlantis?</p>
-      //     </div>
-      //   </div>
-      //   <div className="row">
-      //     <div className="col-md-8">
-      //       <table className="table">
-      //         <tbody>
-      //           <tr className="first-option">
-      //             <td>A</td>
-      //             <td>Eruption of Mount Vesuvius</td>
-      //           </tr>
-      //           <tr className="second-option">
-      //             <td>B</td>
-      //             <td>The siege of Troy</td>
-      //           </tr>
-      //           <tr className="third-option">
-      //             <td>C</td>
-      //             <td>The minoan eruption of Santorini</td>
-      //           </tr>
-      //           <tr className="fourth-option">
-      //             <td>D</td>
-      //             <td>The asteroid that killed the dinosaurs</td>
-      //           </tr>
-      //         </tbody>
-      //       </table>
-      //     </div>
-      //     <div className="col-md-4">
-      //       <h2>Time Limit:</h2>
-      //       <p>20 seconds</p>
-      //     </div>
-      //   </div>
-      // </div>
-      <Router>
-        <div className="container-fluid">
-          <h1>Smart-Ass™</h1>
-          <h2>A Trivia Game</h2>
-          <h3>© 2018</h3>
-          <div className="row">
-            <div className="col-md-12">
-              <div className="row">
-                <div className="col-md-8">
-                  <h3>Leader Board</h3>
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>Rank</th>
-                        <th>Username</th>
-                        <th>Wins</th>
-                        <th>Losses</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Jstrzesz</td>
-                        <td>4</td>
-                        <td>0</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div className="col-md-4">
-                <h3>Log In</h3>
-                  <form role="form">
-                    <div className="form-group">
-                      <label>
-                        Username
-                  </label>
-                      <input type="email" className="form-control" id="UsernameInput" />
-                    </div>
-                    <div className="form-group">
-                      <label>
-                        Email address
-                  </label>
-                      <input type="email" className="form-control" id="EmailInput" />
-                    </div>
-                    <div className="form-group">
-                      <label>
-                        Password
-                  </label>
-                      <input type="password" className="form-control" id="PasswordInput" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                      Submit
-                </button>
-                  </form>
-                  <h4>New Players</h4>
-                  <h5>Not a player? Sign Up Here </h5>
-                  <Link to="/sign_up">
-                    <button type="submit" className="btn btn-primary">Sign Up</button>
-                    <Route path="/sign_up" component={SignUp} />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Router>
+      <Home />
     )
   }
 }
