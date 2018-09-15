b77eb9ac04a16a24138b8937ded468a50ca10f3e
jvalamis <jvalamis@gmail.com>
2018-09-15 15:08:55 -0500

    signuppseudo2

--- front-end/src/components/sign_up.jsx
+++ front-end/src/components/sign_up.jsx
@@ -3,66 +3,76 @@ import React from 'react';
 //import axios for http requests
 
 class SignUp extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-      //username: ''
-      //password: ''
-      //re-enter password: ''
-      
+            //username: ''
+            //password: ''
+            //re-enter password: ''
+
+        }
+        this.onClick = this.onClick.bind(this);
     }
-    this.onClick = this.onClick.bind(this);
-  }
 
-  onClick (value) {
-    //axios.post('/signup').then(request, response)
-    this.setState({
-      
-    })
-  }
+    onClick(value) {
+        //axios.post('/signup').then(request, response)
+        // axios.post('/user', {
+        //     firstName: 'Fred',
+        //     lastName: 'Flintstone'
+        // })
+        //     .then(function(response) {
+        //         console.log(response);
+        //     })
+        //     .catch(function(error) {
+        //         console.log(error);
+        //     });
+        this.setState({
 
-  render () {
-    return (
-      <div className="container-fluid">
-        <h1>Smart-Ass Sign Up</h1>
-        <div className="row">
-          <div className="col-md-12">
-            <form role="form">
-              <div className="form-group">
-                <label>Create Username</label>
-                <input type="email" className="form-control" id="UsernameCreate" />
-              </div>
-              <div className="form-group">
+        })
+    }
 
-                <label>
-                  Email address
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Smart-Ass Sign Up</h1>
+                <div className="row">
+                    <div className="col-md-12">
+                        <form role="form">
+                            <div className="form-group">
+                                <label>Create Username</label>
+                                <input type="email" className="form-control" id="UsernameCreate" />
+                            </div>
+                            <div className="form-group">
+
+                                <label>
+                                    Email address
 					</label>
-                <input type="email" className="form-control" id="EmailInputSign-In" />
-              </div>
-              <div className="form-group">
+                                <input type="email" className="form-control" id="EmailInputSign-In" />
+                            </div>
+                            <div className="form-group">
 
-                <label id="passwordInput">
-                  Password
+                                <label id="passwordInput">
+                                    Password
 					</label>
-                <input type="password" className="form-control" id="passwordInputSign-In" />
-              </div>
-              <div className="form-group">
+                                <input type="password" className="form-control" id="passwordInputSign-In" />
+                            </div>
+                            <div className="form-group">
 
-                <label id="passwordInput2">
-                  Re-Enter Password to confirm
+                                <label id="passwordInput2">
+                                    Re-Enter Password to confirm
 					</label>
-                <input type="password" className="form-control" id="passwordInputSign-InConfirm" />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Submit
+                                <input type="password" className="form-control" id="passwordInputSign-InConfirm" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
 				</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default SignUp;
