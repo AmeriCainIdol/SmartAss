71cbf5c22d5152b20290412b94ef1738300585c7
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-15 23:34:52 -0500

    timer starts automatically, stops at 0

--- front-end/src/components/timer.jsx
+++ front-end/src/components/timer.jsx
@@ -4,42 +4,41 @@ class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 0,
-      start: 0
+      timeRemaining: 20
     }
 
-    this.startTimer = this.startTimer.bind(this);
-    this.stopTimer = this.stopTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
+    this.countdown = this.countdown.bind(this);
   }
-  startTimer () {
-    this.setState({
-      time: this.state.time,
-      start: Date.now()
-    })
-    this.timer = setInterval(() => this.setState({
-      time: Date.now() - this.state.time
-    }), 1000)
-    console.log('start')
+  countdown () {
+    this.setState((prevState, props) => ({
+      timeRemaining: prevState.timeRemaining - 1
+    }))
+    if (this.state.timeRemaining === 0) {
+      clearInterval(this.interval);
+    }
+    // this.setState({
+    //   time: this.state.time,
+    //   start: Date.now()
+    // })
+    // this.timer = setInterval(() => this.setState({
+    //   time: this.state.time - 1
+    // }), 1000)
+    // console.log('start')
   }
 
-  stopTimer () {
-    clearInterval(this.timer)
-    console.log('stop')
+  componentDidMount() {
+    this.interval = setInterval(() => this.countdown(), 1000)
   }
 
-  resetTimer () {
-    this.setState({time: 0})
-    console.log('reset')
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   render () {
     return (
       <div>
-        <h3>Time left: {this.state.time}</h3>
-        <button onClick={this.startTimer}>Start</button>
-        <button onClick={this.stopTimer}>Stop</button>
-        <button onClick={this.resetTimer}>Reset</button>
+        <h3>Time Remaining: {this.state.timeRemaining}</h3>
+        {/* <button onClick={this.countdown}>Start</button> */}
       </div>
     )
   }
