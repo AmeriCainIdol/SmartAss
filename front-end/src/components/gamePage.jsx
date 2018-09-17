c453df4ced17b6655b82c3bb9f1517c0a437b7ac
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-09-17 04:40:04 -0500

    created questions and answers pages, each rendering, but not correctly

--- front-end/src/components/gamePage.jsx
+++ front-end/src/components/gamePage.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import Answers from './gamePageAnswers.jsx';
+import Questions from './gamePageQuestions.jsx';
 
 export default class GamePage extends Component {
   constructor(props) {
     super(props);
-    // console.log(props.history.location.state.state);
+    console.log(props.history.location.state.state, 'hello');
     this.state = {
       selectedAnswer: null,
       score: 0,
-      correctAnswer: 'C',
+      correctAnswer: null,
       timeRemaining: 20,
       questionsToDisplay: this.props.history.location.state.state,
       newQuestionOrder: []
@@ -68,33 +70,22 @@ export default class GamePage extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-12">
-            <h2>Question #</h2>
-            <p>{this.state.questionsToDisplay[0].question}</p>
+            {this.state.questionsToDisplay.map((question, index) => {
+              console.log(question, 'ugh')
+              return (<Questions key={index} question={question}/>)
+            })}
           </div>
         </div>
         <div className="row">
           <div className="col-md-8">
             <table className="table">
-              <tbody>
-                <tr className="first-option">
-                  <td style={{ background: this.selectedColor('A') }} onClick={() => this.toggleSelector('A')}>A</td>
-                  <td style={{ background: this.selectedColor('A') }} onClick={() => this.toggleSelector('A')}></td>
-                </tr>
-                <tr className="second-option">
-                  <td style={{ background: this.selectedColor('B') }} onClick={() => this.toggleSelector('B')}>B</td>
-                  <td style={{ background: this.selectedColor('B') }} onClick={() => this.toggleSelector('B')}>The siege of Troy</td>
-                </tr>
-                <tr className="third-option">
-                  <td style={{ background: this.selectedColor('C') }} onClick={() => this.toggleSelector('C')}>C</td>
-                  <td style={{ background: this.selectedColor('C') }} onClick={() => this.toggleSelector('C')}>The minoan eruption of Santorini</td>
-                </tr>
-                <tr className="fourth-option">
-                  <td style={{ background: this.selectedColor('D') }} onClick={() => this.toggleSelector('D')}>D</td>
-                  <td style={{ background: this.selectedColor('D') }} onClick={() => this.toggleSelector('D')}>The asteroid that killed the dinosaurs</td>
-                </tr>
-              </tbody>
+              {this.state.questionsToDisplay.map((question, index) => {
+                console.log(question, 'ugh')
+                return (<Answers key={index} question={question} />)
+              })}
             </table>
           </div>
+          
           <div className="col-md-4">
             <div>
               <h3>Time Remaining: {this.state.timeRemaining}</h3>
